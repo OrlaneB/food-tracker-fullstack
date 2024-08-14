80bc4cd7bff3d96e27c300cfd77f1a3002299c7d
Orlane <148352870+OrlaneB@users.noreply.github.com>
2024-08-14 19:04:13 +0200

    Dynamic pie charts

--- client/src/components/BarGraph.jsx
+++ client/src/components/BarGraph.jsx
@@ -1,18 +1,24 @@
 /* eslint-disable react/prop-types */
 import '../styles/BarGraph.css'
 
-export default function BarGraph({objDay}) {
-    //const [test,setTest] = useState({});
-    // const [count,setCount] = useState([1,2,3])
-    // copy dummy data
+export default function BarGraph({objDay,percentage}) {
+
     const objDay1= objDay;
     const {protein, carbs, fat} = objDay1;
 
+    
+
+
+
     return (
       <div className = "BarGraph">
-        <p>{protein}</p> 
-        <p>{carbs}</p>
-        <p>{fat}</p>
+        {percentage.map((nut,index)=>(
+          <div className='pie' key={index} style={{backgroundImage:`conic-gradient(lightblue ${nut.percentage*3.6}deg,
+          rgb(89, 89, 89) 0)`}}>
+            <div className='text'><p>{nut.percentage}</p></div>
+            <p className='nutrientName' >{nut.name}</p>
+          </div>
+        ))}
       </div>
     )
 }
--- client/src/components/Homepage.jsx
+++ client/src/components/Homepage.jsx
@@ -19,11 +19,6 @@ let objMeal1 = {
   carbs : '15g',
   fat : "1g"
 }
-
-/* Calculate the nutrient in a method inside the meal object
- {proteinCopy+=item.numberAmount}
-
-*/
 let objMeal2 = {
   ingredients : [
     {ingredientName : "rice", numberAmount : "1", measurement : "portion", protein : "x g", carbs : "y g", fat : "z g" }],
@@ -31,7 +26,6 @@ let objMeal2 = {
   carbs : '15g',
   fat : "12g"
 }
-
 let objMeal3 = {
   ingredients : [
     {ingredientName : "rice", numberAmount : "1", measurement : "portion", protein : "x g", carbs : "y g", fat : "z g" }],
@@ -39,8 +33,6 @@ let objMeal3 = {
   carbs : '5g',
   fat : "8g"
 }
-
-// Meals for the day
 let objDay = {
   meals : [objMeal1, objMeal2, objMeal3],
   protein : '37',
@@ -49,11 +41,32 @@ let objDay = {
 }
 
 
+
+
 export default function Homepage() {
+
+  const [day,setDay]=useState(new Date())
+
+  //Will come from AuthContext
+  const isLoggedIn = true;
+
+  let dummyData = [
+    {name:"Protein", percentage:84},
+    {name:"Vitamin A, RAE", percentage:24},
+    {name:"Iron, Fe",percentage:56}];
+
+  const [nutrientPercentage,setNutrientPercentage] = useState(dummyData);
+
+      
+      
+
   return (
     <div className='Homepage'>
-      <Day />
-      <BarGraph objDay = {objDay}/>
+
+      
+
+      <Day dateObj={{day,setDay}}/>
+      <BarGraph objDay = {objDay} percentage={nutrientPercentage}/>
       <MealCards objDay = {objDay}/>
 
     </div>

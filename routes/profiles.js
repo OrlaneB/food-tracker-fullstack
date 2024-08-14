55a08dff7556a3a22494045c7cd3a21eb95c1141
Laura Castro <lauracastrotech@gmail.com>
2024-08-14 14:05:19 -0400

    added pseudocode for UPDATE query

--- routes/profiles.js
+++ routes/profiles.js
@@ -3,7 +3,7 @@ var router = express.Router();
 const userShouldBeLoggedIn = require("../guards/userShouldBeLoggedIn")
 
 /* GET profile information
-add middleware to check id userIdMustExist
+add middleware to check id userShouldBeLoggedIn
 */
 router.get("/",userShouldBeLoggedIn,async(req, res)=>{
   console.log(res.locals.userId);
@@ -16,6 +16,13 @@ add middleware to check id userIdMustExist
 */
 
 /*UPDATE profile information*/
+// create array of the column names and use index to add to loop
+// from the frontend i get an array that has 0-3 nutrients, username
+// i want to update the nutrients when username
+// loop with 3 iterations for each if there is something in the array add the nutrient to nutrient_1 = index+1, etc.
+// else set the nutrient to null
+
+
 
 /* DELETE account, will need to figure out if its tied to userid or profile id, whichever you decide you will need ON DELETE CASCADE*/
 

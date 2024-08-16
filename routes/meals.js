c782bb5cb708cf5b58a5d4ee5293d91ac3f5d455
Laura Castro <lauracastrotech@gmail.com>
2024-08-16 16:19:09 -0400

    added get meals endpoint

--- routes/meals.js
+++ routes/meals.js
@@ -4,32 +4,33 @@ const db = require('../model/helper');
 const userShouldBeLoggedIn = require("../guards/userShouldBeLoggedIn")
 
 /* GET meals*/
-// router.get("/:date",userShouldBeLoggedIn, async(req, res)=>{
-router.get("/:profile_id", async(req, res)=>{
+router.get("/:profile_id:date", async(req, res)=>{
 
-  const { date, profile_id } = req.params;
-  console.log("profile_id", profile_id);
+  const { date, profile_id} = req.params;
+  // console.log(req.params);
+  // console.log("profile_id", profile_id);
+  // console.log("date of meal", date.slice(0,10));
 
   if (!profile_id) {
     return res.status(400).send({ error: "Profile ID is required!" });
   }
+
   try {
+    // Join ingredients, meals, and nutrients_by_meal tables 
     const results = await db(`SELECT * 
-                              FROM meals 
-                              LEFT JOIN ingredients
-                              LEFT JOIN nutrients_by_meal
-                              ON meals.meal_id = 1
-                              WHERE meals.profile_id = ${profile_id},
-                              AND date = "${date.slice(0,10)}"`);
+                              FROM ingredients 
+                              RIGHT JOIN meals ON meals.meal_id = ingredients.meal_id 
+                              RIGHT JOIN nutrients_by_meal ON meals.meal_id = nutrients_by_meal.meal_id
+                              WHERE meals.profile_id = ${profile_id}
+                              AND meals.date = "${date.slice(0,10)}"
+                            `);
 
     if (results.length === 0) {
       return res.status(404).send({ error: "Meal not found!" });
     } else {
-      // Initialize result object
-      let resObj = results.data[0]
-
-      // Add success message
-      res.status(200).send({message:'Success', resObj});
+      const data = results.data;
+      // Return data and add success message
+      res.status(200).send({message:'Success', data});
     }
   } catch (e) {
     console.log("something happened");

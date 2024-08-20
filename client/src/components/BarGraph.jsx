052eb5c852889016b64c56bf497ab70406e03a3b
Orlane <148352870+OrlaneB@users.noreply.github.com>
2024-08-20 17:18:21 +0200

    Bar Graph works dynamically

--- client/src/components/BarGraph.jsx
+++ client/src/components/BarGraph.jsx
@@ -1,5 +1,7 @@
 /* eslint-disable react/prop-types */
+import { useContext, useEffect, useState } from 'react';
 import '../styles/BarGraph.css'
+import mealsForOneDate from '../context/mealsForOneDate';
 
 export default function BarGraph({objDay,percentage}) {
 
@@ -8,17 +10,61 @@ export default function BarGraph({objDay,percentage}) {
 
     const colors = ["#99C8FF","#2866b2","#6880FF"]
 
+    const recommendations = {
+      Protein:120,
+      "Iron, Fe":180,
+      "Vitamin A, RAE":2500
+    }
+
+    const {nutrientsByMeal} = useContext(mealsForOneDate);
+    const [nutrients,setNutrients] = useState();
+
+    function calculatePercentage(){
+
+        if(nutrientsByMeal){let allNutrients = [...nutrientsByMeal].flat();
+
+        let listNutrients = {}
+
+        allNutrients.forEach(nut=>{
+          if(!Object.keys(listNutrients).includes(nut.nutrient_name)){
+            listNutrients[nut.nutrient_name]=nut.nutrient_number_amount;
+          }else {
+            listNutrients[nut.nutrient_name]+=nut.nutrient_number_amount;
+          }
+        })
+
+        let nutrientsPercentage = [];
+
+        for(let nutName in listNutrients){
+          let percentage = (100*listNutrients[nutName])/recommendations[nutName];
+
+          nutrientsPercentage.push({nutrient_name:nutName,current:listNutrients[nutName],percentage})
+        }
+
+        setNutrients(nutrientsPercentage);
+
+        
+      }
+
+    }
+
+    useEffect(()=>{
+      calculatePercentage();
+    },[nutrientsByMeal]);
 
 
     return (
       <>
         <h2 style={{textAlign:"center",margin:"0"}}>Your nutrients for today</h2>
         <div className = "BarGraph">
-        {percentage.map((nut,index)=>(
-          <div className='pie' key={index} style={{backgroundImage:`conic-gradient(${colors[index]} ${nut.percentage*3.6}deg,
-          black 0)`}}>
-            <div className='text'><p>{nut.percentage}</p></div>
-            <p className='nutrientName' >{nut.name}</p>
+        {nutrients && nutrients.map((nut,index)=>(
+          <div className='pie' key={index} style={nut.percentage<50?
+            {backgroundImage:`conic-gradient(${colors[index]} 0deg ${nut.percentage * 3.6}deg, black ${nut.percentage * 3.6}deg 360deg)`} :
+            {backgroundImage:`conic-gradient(${colors[index]} 0deg ${nut.percentage === 100 ? 360 : nut.percentage * 3.6}deg, black ${nut.percentage * 3.6}deg 360deg)`}}>
+            <div className='text' >
+                    <p>{nut.current}g <br/> 
+                    <span style={{color:"grey",fontSize:"0.7em"}}>/{recommendations[nut.nutrient_name]}g</span></p></div>
+            <p className='nutrientName' >{nut.nutrient_name}</p>
           </div>
         ))}
         </div>

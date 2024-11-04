89aa8bb6305453657954e08fe847654a44c91666
Orlane <148352870+OrlaneB@users.noreply.github.com>
2024-11-04 16:26:47 +0100

    Built the client

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 // Serve static files from the React frontend app
-// app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'client/dist')));
 
 app.get("/", function(req, res, next) {
   res.send("Access the API at path /api");
@@ -35,7 +35,7 @@ app.use('/api/meals', mealsRouter);
 
 // Anything that doesn't match the above, send back index.html
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname + "/client/build/index.html"));
+  res.sendFile(path.join(__dirname + "/client/dist/index.html"));
 });
 
 // catch 404 and forward to error handler

a2e50bf92af19f6ecfa3231b1f890b29dd29a983
Orlane <148352870+OrlaneB@users.noreply.github.com>
2024-11-04 16:12:29 +0100

    Changed res.render

--- app.js
+++ app.js
@@ -51,7 +51,10 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render('error',{
+    message: err.message,
+    error: process.env.NODE_ENV === 'development' ? err : {}
+  });
 });
 
 module.exports = app;

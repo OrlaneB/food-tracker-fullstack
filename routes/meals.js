f8bcbe31e2f5e27db36ea6debce9c47d4559e5bb
Orlane <148352870+OrlaneB@users.noreply.github.com>
2024-11-25 12:25:40 +0100

    Refactored meals GET

--- routes/meals.js
+++ routes/meals.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var router = express.Router();
 const db = require('../model/helper');
-// const userShouldBeLoggedIn = require("../guards/userShouldBeLoggedIn")
 
 const nutrientNames = [ "Energy","Protein", "Carbohydrate, by difference","Total lipid (fat)","Fiber, total dietary","Total Sugars","Calcium, Ca","Iron, Fe","Potassium, K","Sodium, Na","Vitamin A, RAE","Vitamin C, total ascorbic acid","Vitamin D (D2 + D3)","Vitamin E (alpha-tocopherol)","Vitamin K (phylloquinone)","Magnesium, Mg","Zinc, Zn","Cholesterol","Folate, DFE","Fatty acids, total polyunsaturated" ]
 
@@ -24,41 +23,36 @@ function checkObjectFormat(obj, type) {
 }
 
 /* GET meals, ingredients, nutrients*/
-// router.get("/:profile_id", userShouldBeLoggedIn, async(req, res)=>{
-router.get("/:profile_id", async(req, res)=>{
-  const {date} = req.query; // recommended to not use req.body with a get request
-  const {profile_id}= req.params;
-
-  if (!profile_id) {
-    return res.status(400).send({ error: "Profile ID is required!" });
-  }
+router.get("/:profile_id/:date", async(req, res)=>{
+  const {profile_id,date}= req.params;
 
   try {
-    // Join ingredients, meals, and nutrients_by_meal tables 
-    const resultsIngredients = await db(`SELECT *
-                              FROM meals m 
-                              JOIN ingredients i ON m.meal_id = i.meal_id 
-                              WHERE m.profile_id = ${profile_id}
-                              AND m.date = "${date.slice(0,10)}"
-                            `);
-    const resultsNutrients = await db(`SELECT *
-                              FROM meals m 
-                              JOIN nutrients_by_meal n ON m.meal_id = n.meal_id 
-                              WHERE m.profile_id = ${profile_id}
-                              AND m.date = "${date.slice(0,10)}"
-                            `);
+    const result = await db(
+      "SELECT COUNT(*) AS profile_count FROM profiles WHERE profile_id= ? ;",
+      [profile_id]
+    )
 
-    if (resultsIngredients.length === 0 || resultsNutrients.length === 0 ) {
-      return res.status(404).send({ error: "Meal not found!" });
-    } else {
-      const dataIngredients = resultsIngredients.data;
-      const dataNutrients = resultsNutrients.data;
-      // Return data and add success message
-      res.status(200).send({message:'Success', dataIngredients, dataNutrients});
-    }
-  } catch (e) {
-    console.log("something happened");
-    res.status(500).send({ error: e.message });
+    if(result.data[0].profile_count !== 1) return res.status(404).json({message:"This profile does not exists."});
+
+    const mealsResult = await db(
+      "SELECT meal_id, nutrients, ingredients FROM MEALS WHERE profile_id= ? AND date = ? ;",
+      [profile_id,date]
+    )
+
+    const meals = mealsResult.data.map((m) => ({
+      [m.meal_id]: JSON.parse(m.ingredients)
+    }));
+
+    const nutrients = mealsResult.data.map((m)=> ({
+      [m.meal_id] : JSON.parse(m.nutrients)
+    }));
+  
+
+    res.status(200).json({message:"Successful meal retrieval.",meals,nutrients});
+
+  } catch(err){
+    console.error("Error retrieving meals",err);
+    res.status(500).json({message:"Error while retrieving meals : ",err})
   }
 
 })
@@ -69,7 +63,6 @@ router.post('/:profile_id', async(req, res) => {
   const { date, nutrients, ingredients} = req.body;
 
   try {
-    // if(checkObjectFormat(nutrients,"nutrients") && checkObjectFormat(ingredients,"ingredients")){
     if(!checkObjectFormat(nutrients,"nutrients")) return res.status(400).json({message:"Invalid nutrients format."});
     if(!checkObjectFormat(ingredients,"ingredients")) return res.status(400).json({message:"Invalid ingredients format."});
 
@@ -80,10 +73,6 @@ router.post('/:profile_id', async(req, res) => {
 
       res.status(201).json({message:"Meal created successfully"});
 
-
-    // } else {
-    //   return res.status(401).json({message:"Nutrients or ingredients don't have the correct format."});
-    // }
   } catch(err){
     console.error("Error creating meal : ",err);
     res.status(500).json({message:"An error occured during meal creation.",err});

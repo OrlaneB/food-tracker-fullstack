4d4ae555861e2dcadaef7e752317953f3a8eebb2
Orlane <148352870+OrlaneB@users.noreply.github.com>
2025-01-05 18:21:36 +0100

    Moved checkToken function from App.jsx to utilities

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,18 +5,18 @@ import AddMeal from './components/AddMeal.jsx'
 
 import './App.css'
 
-import axios from 'axios'
 import { BrowserRouter as Router, Routes, Route, useNavigate } from 'react-router-dom';
 
 import Register from './components/Register.jsx';
 import Login from './components/Login.jsx';
 import Profile from './components/Profile.jsx';
-import SurveyLink from './components/layout/SurveyLink.jsx';
 import ReportAnIssue from './components/ReportAnIssue.jsx';
 
 import profileInfoContext from './context/profileInfo.jsx';
 import LearnAboutProject from './components/LearnAboutProject.jsx'
 import Layout from './components/layout/Layout.jsx'
+
+import checkToken from "./utilities/authentification/checkToken.js"
 
 
 function App() {
@@ -29,38 +29,18 @@ function App() {
   });
 
 
-  async function checkToken(){
-    const token = localStorage.getItem("token");
+  async function toCheckToken(){
+    const profileInfo = await checkToken();
 
-    if(token){
-      try{
-
-        const response = await axios.post(`${import.meta.env.VITE_URL_REQUESTS}/api/users/token`,{
-          token
-        });
-
-        if(response.statusText==="OK") {
-          console.log(response.data.message);
-          // console.log("profile info : ",response.data.profileInfo);
-          setProfileInfo(response.data.profileInfo);
-        } else {
-          console.log("Token is uncorrect")
-          navigate("/login");
-        }
-
-      }
-      catch(err){
-        console.log(err);
-        navigate("/login");
-      }
+    if(profileInfo){
+      setProfileInfo(profileInfo)
     } else {
-      console.log("There's no token");
-      navigate("/login");
+      navigate("/login")
     }
   }
 
   useEffect(()=>{
-    checkToken()
+    toCheckToken()
   },[])
 
 

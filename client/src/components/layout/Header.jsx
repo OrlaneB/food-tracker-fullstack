ecfd4e77a63188bd8f776b9212883438f4aefb5f
Orlane <148352870+OrlaneB@users.noreply.github.com>
2025-01-16 15:54:33 +0100

    Fixed navigation

--- client/src/components/layout/Header.jsx
+++ client/src/components/layout/Header.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
 import DarkLightThemeButton from './DarkLightThemeButton';
+import "../../styles/Header.css"
 
 export default function Header() {
 
